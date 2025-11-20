a2c670aedcb041b66c5a4df2cd7e247861b6af3a
arthurrogeriozip <arthur.mendonca@sptech.school>
2025-11-20 19:51:49 -0300

    Alteração login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -19,20 +19,7 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoFortnite) => {
-                                if (resultadoFortnite.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha
-                                    });
-                                } else {
-                                    res.status(204).json({ fortnite: [] });
-                                }
-                            })
+                        res.json(resultadoAutenticar);
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
